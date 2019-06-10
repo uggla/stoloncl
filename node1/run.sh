c3eaae921885affc436d7934a554659f442d4b96
Uggla <uggla@uggla.fr>
2019-06-10 01:18:42 +0000

    Update

--- node1/run.sh
+++ node1/run.sh
@@ -4,15 +4,8 @@ set -euo pipefail
 set -x
 IFS=$'\n\t'
 
-wait_rabbit() {
-	while ! curl -s localhost:15672 | grep "title" | grep "RabbitMQ Management"
-	do
-		sleep 5s
-	done
-}
-
-if [[ -f data ]]; then
-	rm -rf data pgdata
+if [[ -f remove ]]; then
+	rm -rf data pgdata remove
 fi
 
 node1=$(nslookup node-1.uggla.fr | sed -rn '/Name/,/Address/'p | grep "Address" | awk '{print $NF}')
@@ -25,18 +18,16 @@ iam=$(ip a s eth0 | grep inet | head -1 | awk '{print $2}' | sed -r 's#/[0-9]+##
 docker-compose -f docker-compose-etcd.yml up -d
 
 # Check etcd
-while [[ "$(docker exec node1_etcd_1 /bin/sh -c "ETCDCTL_API=3 /usr/local/bin/etcdctl member list" | grep -c "node")" != "3" ]]
+while [[ "$(docker exec $(docker ps | grep etcd | awk '{print $NF}') /bin/sh -c "ETCDCTL_API=3 /usr/local/bin/etcdctl member list" | grep -c "node")" != "3" ]]
 do
 	sleep 5s
 done
 
 mkdir -p pgdata
 chown -R 1000:1000 pgdata
+# Init db if required and node 1
 if [[ ! -f init.done ]] && [[ "${iam}" == "${node1}" ]]; then
 	docker run -ti sorintlab/stolon:master-pg10 stolonctl --cluster-name stolon-cluster --store-backend=etcdv3 --store-endpoints http://node-1.uggla.fr:2379,http://node-2.uggla.fr:2379,http://node-3.uggla.fr:2379 init --yes && touch init.done
 fi
-docker-compose -f docker-compose-stolon.yml up -d
 
-exit 0
-gosu stolon stolon-sentinel --cluster-name stolon-cluster --store-backend=etcdv3 --store-endpoints http://node-1:2379,http://node-2:2379,http://node-3:2379 --log-level debug
-docker run -ti sorintlab/stolon:master-pg10 stolonctl --cluster-name stolon-cluster --store-backend=etcdv3 --store-endpoints http://node-1.uggla.fr:2379,http://node-2.uggla.fr:2379,http://node-3.uggla.fr:2379 init --yes
+docker-compose -f docker-compose-stolon.yml up -d
